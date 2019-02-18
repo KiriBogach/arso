57692c0be7ecac595bb7602c78d73d7c1d527389
MarcosMenarguez <marcos.menarguez@gmail.com>
2019-02-18 10:00:23 +0100

    Ejercicios 1.4 y 1.5 revisados (bien).

--- src/sax/ejercicio1_5/SAX.java
+++ src/sax/ejercicio1_5/SAX.java
@@ -8,6 +8,10 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.SAXException;
 
+/*
+ * Revisión: bien.
+ */
+
 public class SAX {
 
 	public static void main(String[] args) throws ParserConfigurationException, SAXException {
