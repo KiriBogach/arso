94a36d0dcb45ef6f59a104856bf8d8ef372a67c7
MarcosMenarguez <33449486+MarcosMenarguez@users.noreply.github.com>
2019-05-13 12:13:54 +0200

    Ejercicio 2.2 revisado

--- conversor-soap/src/soap/Conversor.java
+++ conversor-soap/src/soap/Conversor.java
@@ -2,6 +2,9 @@ package soap;
 
 import javax.jws.WebService;
 
+// El servicio no se publica correctamente en Tomcat
+// En web.xml ha faltado asociar la URL con el servlet
+
 @WebService
 public interface Conversor {
 	
